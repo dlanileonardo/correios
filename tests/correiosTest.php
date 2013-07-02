28245a4e018ceb037a27544123742368c814585e
Dlani Mendes <dlanileonardo@gmail.com>
2013-07-02 03:00:21 -0300

    Indentação

--- tests/correiosTest.php
+++ tests/correiosTest.php
@@ -2,10 +2,10 @@
 
 class correiosTest extends PHPUnit_Framework_TestCase {
 
-	/**
-	* Testa o construtor da classe
-	*/
-	public function testConstructClass()
+    /**
+    * Testa o construtor da classe
+    */
+    public function testConstructClass()
     {
         $correios = new correios();
         $this->assertTrue(is_object($correios) && $correios instanceof correios);
